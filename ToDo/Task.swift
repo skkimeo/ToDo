72e45879b0884adef1af3b66e7a9e38eb5257daa
sun <skkimeo@gmail.com>
2021-10-26 16:14:27 +0900

    created the model for each task

---
+++ ToDo/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  ToDo
+//
+//  Created by sun on 2021/10/26.
+//
+
+import Foundation
+
+struct Task {
+    var name: String
+    // or maybe the description could be some type of array?
+    // so that I can display them in list?
+    var description: String
+    var date: Date
+}
