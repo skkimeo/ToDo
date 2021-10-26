9e5c803c1a52ac0479ebabefd95828346676640f
sun <skkimeo@gmail.com>
2021-10-26 16:03:27 +0900

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by sun on 2021/10/26.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
